9ffe5b866b98bf26ef4309cb7175238c59173319
明扬 <my@famer.me>
2021-11-12 00:00:13 +0800

    [PoC] Test packaging python into dist file.

--- buildSrc/src/main/kotlin/dev/famer/build/DownloadPython.kt
+++ buildSrc/src/main/kotlin/dev/famer/build/DownloadPython.kt
@@ -12,11 +12,14 @@ import io.ktor.utils.io.*
 import io.ktor.utils.io.core.*
 import kotlinx.coroutines.runBlocking
 import java.io.File
+import java.nio.ByteBuffer
 import java.nio.channels.AsynchronousFileChannel
 import java.nio.channels.Channels
 import java.nio.channels.FileChannel
 import java.nio.file.Files
+import java.nio.file.OpenOption
 import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
 import java.util.concurrent.Future
 
 open class DownloadPythonTask : DefaultTask() {
@@ -31,12 +34,24 @@ open class DownloadPythonTask : DefaultTask() {
     private suspend fun impl() {
         val client = HttpClient(CIO)
 
-        val file = Files.createTempFile("python", "zip")
-        val ch = FileChannel.open(file)
+        val file = Files.createTempFile("python", ".zip")
+        println(file.toRealPath())
+        val ch = AsynchronousFileChannel.open(file, StandardOpenOption.WRITE)
 
         client.get<HttpStatement>("https://www.python.org/ftp/python/3.10.0/python-3.10.0-embed-amd64.zip")
-            .execute { response: HttpResponse ->
-                val channel: ByteReadChannel = response.receive()
+            .execute {
+                var offset = 0
+                val byteBufferSize = 1024 * 100
+                val channel: ByteReadChannel = it.receive()
+                it.contentLength()?.let { contentLength ->
+                    val data = ByteArray(contentLength.toInt())
+                    do {
+                        val currentRead = channel.readAvailable(data, offset, byteBufferSize)
+                        ch.write(ByteBuffer.wrap(data), offset.toLong()).get()
+                        offset += currentRead
+                    } while (currentRead >= 0)
+
+                }
             }
     }
 }
